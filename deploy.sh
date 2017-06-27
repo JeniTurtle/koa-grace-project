5bfec6a9fb9c774fe0d52b39c1d8482313a4b604
fanzhongkai <kaizhongfan@163.com>
2017-06-27 16:10:53 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 case $1 in
 pc)
-	pc_root=/home/work/koa_base/source/apps/pc
-    pc_code=/home/work/koa-pc/source/
+    pc_root=/home/work/koa_base/source/apps/pc
+    pc_code=/home/work/koa-pc/source
 
     cp -r $pc_code/* $pc_root
 

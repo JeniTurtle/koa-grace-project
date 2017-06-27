592a37f100eba5bf7d3e6e5d50e135c3d87d1c68
TomorrowCat <1002563923@qq.com>
2017-06-27 19:53:19 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 case $1 in
 pc)
-    pc_root=/home/work/node/servers/koa_base/source/apps/pc
-    pc_code=/home/work/node/servers/koa_pc/source
+    pc_root=/home/work/node/servers/koa_base/current/apps/pc
+    pc_code=/home/work/node/servers/koa_pc/current
 
     cp -r $pc_code/* $pc_root
 

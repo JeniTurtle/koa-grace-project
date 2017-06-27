144aea91fb1b2fda981888145cc0b17c4b8a003b
TomorrowCat <1002563923@qq.com>
2017-06-27 15:50:50 +0800

    update process.json

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,15 @@
-pc_root=/home/work/koa_base/source/apps/pc
-pc_code=/home/work/koa-pc/source/
+case $1 in
+pc)
+	pc_root=/home/work/koa_base/source/apps/pc
+    pc_code=/home/work/koa-pc/source/
 
-echo "更新代码，y or n？"
-read ANS
-case $ANS in
-y)
-	cd $pc_code
-	git pull origin master
-	echo "get koa-pc master done"
+    cp -r $pc_code/* $pc_root
 
-	echo "输入y回车继续，输入n回车结束"
-	read ANS
-	case $ANS in
-	y|Y|yes|Yes)
-		;;
-	n|N|no|No)
-		echo exit
-		exit 0
-		;;
-	esac
+    cd $pc_root && npm install
+
 	;;
-n)
+mobile)
+
 	;;
 esac
 
-cp -r $pc_code/* $pc_root
-
-cd $pc_root && npm install

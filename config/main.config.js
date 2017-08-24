7d77e8893dda3a43a1564ca61e95c6976c1908c3
TomorrowCat <1002563923@qq.com>
2017-08-24 15:19:59 +0800

    update config

--- config/main.config.js
+++ config/main.config.js
@@ -13,13 +13,18 @@ module.exports = {
   extend: true,
 
   vhost: {
-    '127.0.0.1': 'pc/dci',
-    'localhost': 'pc/dci',
-    'testdci.yunlaiwu.com': 'pc/dci',
-    'dci.yunlaiwu.com': 'pc/dci',
-    'banquanbaohu.com': 'pc/dci',
-    'www.banquanbaohu.com': 'pc/dci',
-    'chenming.banquanbaohu.com': 'pc/dci'
+    '127.0.0.1': 'pc',
+    'localhost': 'pc',
+
+    'testdci.yunlaiwu.com': 'pc/dci', // 版保测试服务器域名
+    'dci.yunlaiwu.com': 'pc/dci', // 版保正式环境域名
+    'banquanbaohu.com': 'pc/dci', // 版保正式环境域名
+    'www.banquanbaohu.com': 'pc/dci',  // 版保正式环境域名
+    'chenming.banquanbaohu.com': 'pc/dci', // 版保本地测试域名
+
+    'xinbianju.com': 'pc/newwriter', // 新编剧正式环境域名
+    'www.xinbianju.com': 'pc/newwriter', // 新编剧正式环境域名
+    'chenming.xinbianju.com': 'pc/newwriter' // 新编剧本地测试域名
   },
   
   // proxy timeout时间
@@ -38,11 +43,13 @@ module.exports = {
     // project
     project: path.resolve(__dirname, '../apps/'),
     default_path: {
-      'pc/dci': '/dci/home'
+      'pc/dci': '/dci/home',
+      'pc/newwriter': '/newwriter/home'
     },
     // 如果设置jump为false，则当直接访问域名时不重定向到default_path
     default_jump: {
-      'pc/dci': false
+      'pc/dci': false,
+      'pc/newwriter': false
     }
   },
 
